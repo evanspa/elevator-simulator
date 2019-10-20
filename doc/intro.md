a67a05ef3ce3e9edadb46687b7867e42c6aa6c19
Paul Evans <pevans@pauls-mbp.lan>
2019-10-20 08:37:24 -0400

    updated project.clj

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to elevator-simulator
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Simulation for a building elevator."
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.490"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.4.500"]]
   :repl-options {:init-ns elevator-simulator.core})
