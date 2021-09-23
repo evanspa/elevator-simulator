7978a6377fa4396ab008821759c5a65bc4aec33f
Paul Evans <evansp2@gmail.com>
2021-09-22 22:33:03 -0400

    initial add of non-juc impl

---
+++ src/elevator_simulator/nojuc.clj
@@ -0,0 +1,39 @@
+(ns elevator-simulator.nojuc)
+
+(defn spawn-rider
+  [id
+   starting-floor
+   starting-floor-arrival-timestamp
+   dest-floor
+   body-weight
+   is-rational]
+  {:id id
+   :starting-floor starting-floor
+   :starting-floor-arrival-timestamp starting-floor-arrival-timestamp
+   :dest-floor dest-floor
+   :body-weight body-weight
+   :is-rational is-rational})
+
+(defn make-dest-matrix
+  [num-floors]
+  (let [floor-range (map inc (range num-floors))]
+    (for [start-floor floor-range
+          dest-floor floor-range
+          :when (not= start-floor dest-floor)]
+      [start-floor dest-floor])))
+
+(defn make-rider-spawner
+  [num-floors
+   body-min-weight
+   body-max-weight
+   is-rational]
+  (let [dest-matrix (make-dest-matrix num-floors)
+        body-weight-range (range body-min-weight (inc body-max-weight))]
+    (fn []
+      (let [[starting-floor dest-floor] (rand-nth dest-matrix)]
+        (spawn-rider "id-abc"
+                     starting-floor
+                     :now
+                     dest-floor
+                     (rand-nth body-weight-range)
+                     true)))))
